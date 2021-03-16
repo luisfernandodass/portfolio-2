afaf471e29ca0ae9b1bba8bc6ab1641367fac122
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-03-16 18:42:32 -0300

    update

--- js/countDate.js
+++ js/countDate.js
@@ -1,4 +1,42 @@
-function calcular(){
-    var elQuantity = document.getElementById('valueDonation');
-    elQuantity.textContent = 'R$ ' + optionOne;
+
+/*
+function toCount(){
+    var firstDate = document.getElementById('inputDateStart').value; 
+    var lastDate = document.getElementById('inputDateEnd').value;
+    var time = firstDate - lastDate;
+
+    var showTime = document.getElementById('result');
+    showTime.textContent = time + ' dias';
 }
+*/
+
+function calculate(){
+    var firstNumber = document.getElementById('seven').value;
+    
+    
+
+    var lastNumber = document.getElementById('two').value;
+    var time = firstDate - lastDate;
+
+    var showTime = document.getElementById('result');
+    showTime.textContent = time + ' dias';
+}
+
+function addition(){
+    var one = document.getElementById('one').value;
+    var two = document.getElementById('two').value;
+    var three = document.getElementById('three').value;
+    var four = document.getElementById('four').value;
+    var five = document.getElementById('five').value;
+    var six = document.getElementById('six').value;
+    var seven = document.getElementById('seven').value;
+    var eight = document.getElementById('eight').value;
+    var nine = document.getElementById('nine').value;
+    
+
+    var result = 5 + 10;
+
+    var showTime = document.getElementById('result');
+    showTime.textContent = result;
+}
+
