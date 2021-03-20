314dc0364dd122ee8ad2d278dd1857df51efd9ac
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-03-19 23:51:31 -0300

    update

--- js/countDate.js
+++ js/countDate.js
@@ -1,60 +1,8 @@
-
-/*
-function toCount(){
-    var firstDate = document.getElementById('inputDateStart').value; 
-    var lastDate = document.getElementById('inputDateEnd').value;
-    var time = firstDate - lastDate;
-
-    var showTime = document.getElementById('result');
-    showTime.textContent = time + ' dias';
-}
-
-
+// Mostra no visor o resultado
 function calculate(){
-    var firstNumber = document.getElementById('seven').value;
-    
-    
+    var  i = document.getElementById('showResult');
+    i.textContent = 3;
 
-    var lastNumber = document.getElementById('two').value;
-    var time = firstDate - lastDate;
-
-    var showTime = document.getElementById('result');
-    showTime.textContent = time + ' dias';
-}
-
-function numbers(){
-    var one = document.getElementById('one').textContent;
-    var two = document.getElementById('two').textContent;
-    var three = document.getElementById('three').textContent;
-    var four = document.getElementById('four').textContent;
-    var five = document.getElementById('five').textContent;
-    var six = document.getElementById('six').textContent;
-    var seven = document.getElementById('seven').textContent;
-    var eight = document.getElementById('eight').textContent;
-    var nine = document.getElementById('nine').textContent;
-    
-
-    var result = one + two;
-
-    var showTime = document.getElementById('result');
-    showTime.textContent = result;
-}
-*/
-function vis(num) {
-    if (typeof gvisor == 'undefined') {
-        document.container.visor.value = '';
-    }
-    document.container.visor.value = document.container.visor.value + num;
-    gvisor = 1;
-}
-
-// Função que limpa  a calculadora
-function limpar() {
-    document.container.visor.value = '';
-}
-
-// Funçao que executa as operações
-function operacao(operador, valor1, valor2) {
     if (operador == "somar") {
         var valor = parseFloat(valor1) + parseFloat(valor2);
     } else {
@@ -69,4 +17,10 @@ function operacao(operador, valor1, valor2) {
             return (valor);
         }
     }
+}
+
+// Função que limpa  a calculadora
+function erase() {
+    var out = document.getElementById('showResult');
+    out.textContent = '';
 }
