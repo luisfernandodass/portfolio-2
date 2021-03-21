27a65c2dd7f05230fa135b6b1d90ef46755af8c3
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-03-21 20:24:21 -0300

    update

--- js/countDate.js
+++ js/countDate.js
@@ -1,34 +1,87 @@
-// Mostra no visor o resultado
-function calculate(){
-    var  i = document.getElementById('showResult');
-    i.textContent = '12 + 17';
-
-    function j(valor1, operador, valor2){
-        if (operador == "somar") {
-            var valor = parseFloat(valor1) + parseFloat(valor2);
-        } else {
-            if (operador == "subtrair") {
-                var valor = valor1 - valor2;
-            } else {
-                if (operador == "multiplicar") {
-                    var valor = valor1 * valor2;
-                } else {
-                    var valor = valor1 / valor2;
-                }
-                return (valor);
-            }
-        }
-    }
-    
+function getshowResult(){
+	return document.getElementById("showResult-value").innerText;
 }
-function add(value1, operation, value2){
-     value1 = document.getElementById('').value;
-     operation = document.getElementById('').value;
-     value
+
+function printshowResult(num){
+	document.getElementById("showResult-value").innerText = num;
+}
+
+function getOutput(){
+	return document.getElementById("output-value").innerText;
+}
+
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output-value").innerText = num;
+	}
+	else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}	
 }
 
 // Função que limpa  a calculadora
 function erase() {
-    var out = document.getElementById('showResult');
+    var out = document.getElementById('output-value');
     out.textContent = '';
+}
+function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
+	var n = Number(num);
+	var value = n.toLocaleString("en");
+	return value;
+}
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="clear"){
+			printshowResult("");
+			printOutput("");
+		}
+		else if(this.id=="backspace"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){//if output has a value
+				output= output.substr(0,output.length-1);
+				printOutput(output);
+			}
+		}
+		else{
+			var output=getOutput();
+			var showResult=getshowResult();
+			if(output==""&&showResult!=""){
+				if(isNaN(showResult[showResult.length-1])){
+					showResult= showResult.substr(0,showResult.length-1);
+				}
+			}
+			if(output!="" || showResult!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				showResult=showResult+output;
+				if(this.id=="="){
+					var result=eval(showResult);
+					printOutput(result);
+					printshowResult("");
+				}
+				else{
+					showResult=showResult+this.id;
+					printshowResult(showResult);
+					printOutput("");
+				}
+			}
+		}
+		
+	});
+}
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		var output=reverseNumberFormat(getOutput());
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
 }
