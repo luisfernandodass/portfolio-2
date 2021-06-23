5470d2b896ae6460ad1d14e6e3c4c6bf5f859f39
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-06-23 11:32:49 -0300

    up

--- js/translate.js
+++ js/translate.js
@@ -1,3 +1,16 @@
+const buttonTranslate = document.querySelector(".buttonTranslate");
+const textToTranslate = document.querySelector(".text-language-one").value;
+const textTranslated = document.querySelector(".text-language-two");
+
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
+
+buttonTranslate.addEventListener("click", function(){
+  googleTranslateElementInit();
+  console.log(googleTranslateElementInit());
+  console.log(textToTranslate);
+
+  textTranslated.textContent = textToTranslate;
+})
+
